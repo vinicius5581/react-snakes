32e16aa0e6fda17d45e6f6ff3293eb01b1e3d432
Vinicius Santana <santana@Viniciuss-MacBook-Pro.local>
2018-11-24 19:00:21 -0800

    deployment

---
+++ build/precache-manifest.e111e3f6b53492734d7b56c5e47e489e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "29c31c52cbca5cd5ad8f",
+    "url": "/react-snakes/static/css/main.7bbfa36c.chunk.css"
+  },
+  {
+    "revision": "29c31c52cbca5cd5ad8f",
+    "url": "/react-snakes/static/js/main.29c31c52.chunk.js"
+  },
+  {
+    "revision": "4ed411020c525a9e3f1b",
+    "url": "/react-snakes/static/js/1.4ed41102.chunk.js"
+  },
+  {
+    "revision": "b48d6d27fa671fbd540b",
+    "url": "/react-snakes/static/js/runtime~main.b48d6d27.js"
+  },
+  {
+    "revision": "4f33c380db8cf659f6d4ee108e919cd5",
+    "url": "/react-snakes/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.59d117e7c686d0792d758de33b36a08d.js"
+  "/react-snakes/precache-manifest.e111e3f6b53492734d7b56c5e47e489e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-snakes/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
