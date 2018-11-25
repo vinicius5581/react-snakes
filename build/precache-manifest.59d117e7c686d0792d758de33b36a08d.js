a006a6c30f9806b203d93a77b5ba190ac5549e3d
Vinicius Santana <santana@Viniciuss-MacBook-Pro.local>
2018-11-24 18:49:37 -0800

    Build

---
+++ build/precache-manifest.59d117e7c686d0792d758de33b36a08d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c437d8aee5595be5204f",
+    "url": "/static/css/main.7bbfa36c.chunk.css"
+  },
+  {
+    "revision": "c437d8aee5595be5204f",
+    "url": "/static/js/main.c437d8ae.chunk.js"
+  },
+  {
+    "revision": "4ed411020c525a9e3f1b",
+    "url": "/static/js/1.4ed41102.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5ea6ac5e14c7622800730c21c2cdd41b",
+    "url": "/index.html"
+  }
+];
